65ad00e0d87b14f8794edd7ec3b40fef84004750
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-20 23:03:16 +0530

    2

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -29,8 +29,36 @@ const Landing = () => (
 							</span>
 							
 						</button>
+						
 					</div>
 					
+				</div>
+			</div>
+		</div>
+		<div className="w-full h-full bg-back ">
+			<div className="w-full h-full flex pt-[10%] items-center">
+				<div className="drop-shadow-xl">
+					<img src="/images/image1.png" />
+				</div>
+				<div className="px-[10%] items-center space-y-2">
+					<div className="text-neutral-500 font-Roboto tracking-[0.25rem]">
+						Portfolio Website
+					</div>
+					<div className="text-6xl  w-[80%] text-white font-Poppins font-semibold">
+						Self-introduction and guinea pig
+					</div>
+					<div className="pt-10">
+						<button className=" flex rounded-full border bg-white w-[30%] h-[3.5rem] space-x-[10%] justify-center items-center">
+							<span className="text-black text-2xl  font-Roboto">
+								Read More
+							</span>
+							<span className="">
+								<img src="/images/arrow1.png" />
+							</span>
+								
+						</button>
+							
+					</div>	
 				</div>
 			</div>
 		</div>
