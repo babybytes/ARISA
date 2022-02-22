f9749d013bcef96bc8ddb751fc1b351d57d5f374
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-22 17:59:55 +0530

    project1, project2

--- src/components/screens/projects/Project2.tsx
+++ src/components/screens/projects/Project2.tsx
@@ -3,10 +3,203 @@ import React from 'react';
 
 
 const Project2 = () => (
-	<div className="w-full h-full ">
-		
-		Project2
-      </div>
+	<div className="w-full h-full text-white">
+		<div>
+            <div className="w-full h-full flex  flex-col items-center pt-[7%] space-y-5 ">
+                <div className="text-neutral-400 font-Roboto tracking-[0.2rem] text-sm">
+                    Corporate Website
+                </div>
+                <div className="text-8xl w-[70%] text-center  italic font-bold font-Poppins">
+                    Website rebranding for an industrial waste disposer
+                </div>
+                <div className="pt-[3%] ">
+                    <img src="/images/i2.png" />
+                </div>
+            </div>
+        </div>
+        <div className="flex pt-[15%]  w-full justify-around space-x-[5%] ">
+            <div>
+                <div className="text-6xl font-Poppins font-semibold"> 
+                    Summary
+                </div>
+                <div className="pt-[10%] text-neutral-400">
+                    <button className="border flex rounded-full justify-center items-center w-full h-[3.5rem]">
+                        <span className="font-Roboto">
+                            Visit Website
+                        </span>
+                        <span className="ml-[5%]">
+                            <img src="/images/arrow2.png" />
+                        </span>
+                    </button>
+                </div>
+                
+            </div>
+            <div className="w-[32%]  font-Roboto text-neutral-400 leading-loose">
+                <p className="w-[85%]">
+                    M.O.C. Co., Ltd. is participating in environmental preservation through recycling waste oil and transportation of industrial waste. We rebranded their corporate website to break the image of the traditional manufacturing industry and impress sustainability. 
+                </p>
+            </div>  
+        </div>    
+            <div className="flex justify-around w-full pt-[15%]">
+                <div className="flex flex-col">
+                    <div>
+                        <div className="font-Poppins text-4xl font-semibold tracking-wide">
+                            My Role
+                        </div>
+                        <ul className="pt-5 space-y-3 text-neutral-500 font-Roboto">
+                            <li>
+                                Analysis
+                            </li>
+                            <li>
+                                Information Architecture
+                            </li>
+                            <li>
+                                Worksop
+                            </li>
+                            <li>
+                                Web design
+                            </li>
+                            <li>
+                                Coding
+                            </li>
+                        </ul>
+                    </div>
+                   <div className="pt-[30%] ">
+                        <div className="font-Poppins text-4xl font-semibold tracking-wider">
+                            Technology
+                        </div>
+                        <ul className="pt-5 space-y-3 text-neutral-500 font-Roboto">
+                            <li>
+                                Figma
+                            </li>
+                            <li>
+                                HTML
+                            </li>
+                            <li>
+                                CSS
+                            </li>
+                            <li>
+                                javascript
+                            </li>
+                            <li>
+                                Wordpress
+                            </li>
+                        </ul>
+                   </div>
+                   <div className=" pt-[30%] ">
+                        <div className="font-Poppins text-4xl font-semibold tracking-wider">
+                            Client
+                        </div>
+                        <ul className="pt-5 space-y-3 text-neutral-500 font-Roboto">
+                            <li>
+                                M.O.C Co. Ltd
+                            </li>
+                        </ul>
+                   </div>
+                    
+                </div>
+                <div className="w-[40%] ">
+                    <img src="/images/fish.png" />
+                </div>
+            </div>
+            <div className="ml-[12.7%] font-Poppins text-4xl font-semibold tracking-wider pt-[5%]">
+                Process
+            </div>
+                <div className="flex    pt-[5%]">
+                    <div className="ml-[12.7%]">
+                        <div className="font-Roboto ">
+                            Research
+                        </div>
+                        <div className="font-Roboto ">
+                            Research
+                        </div>
+                        <div className="font-Roboto ">
+                            Research
+                        </div>
+                    </div>
+                    <div className="ml-[20%]">
+                        <div className="text-3xl font-Poppins font-light tracking-wide">
+                            Research
+                        </div>
+                        <div className="font-Roboto pt-5 w-[40%] text-neutral-500">
+                            <p>
+                            First we understood the company, competitors, and their clients by researching and hearing. 
+                            </p>
+                        </div>
+                        <div className=" pt-10 w-[43%]">
+                            <img src="/images/p2a.png" />
+                        </div>
+                    </div>
+                </div>
+                <div className="ml-[36.2%]">
+                    <div className="pt-[15%]">
+                        <div className="text-3xl font-Poppins font-light tracking-wide">
+                            Scope
+                        </div>
+                        <div className="font-Roboto pt-5 w-[43%] text-neutral-500">
+                            <p>
+                                We held workshop to plan what we convey to whom. In this rebranding we set new persona to target, and drew their customer journey to figure out touch points.
+                            </p>
+                        </div>
+                        <div className=" pt-10 w-[43%]">
+                            <img src="/images/p2b.png" />
+                        </div>
+                    </div>
+                    <div className="pt-[10%]">
+                        <div className="text-3xl font-Poppins font-light tracking-wide">
+                             Identity
+                        </div>
+                        <div className="font-Roboto pt-5 w-[43%] text-neutral-500">
+                            <p>
+                                To approach our target with the image of "sustainability", I suggested some ideas with mood boards and came to a conclusion with option 1.
+                            </p>
+                        </div>
+                        <div >
+                            <div className=" pt-10 w-[43%]">
+                                <img src="/images/p2c.png" />
+                            </div>
+                            <div className="font-Roboto pt-5  text-neutral-500 ml-[12%] ">
+                                Clean,Elaborate, Earnest, Conservative
+                            </div>
+                        </div>
+                        <div>
+                            <div className=" pt-10 w-[43%]">
+                                <img src="/images/p2d.png" />
+                            </div>
+                            <div className="font-Roboto pt-5  text-neutral-500 ml-[12%]">
+                                Simple, Modern, Friendly, Easy
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div className="pt-[10%]">
+                        <div className="text-3xl font-Poppins font-light tracking-wide">
+                            Communication
+                        </div>
+                        <div className="font-Roboto pt-5 w-[42%] text-neutral-500">
+                            <p>
+                                I designed entire pages as the sum total of the decisions we have made. I determined how does the product will look like, and choosing the right layout, typography, colors.
+                            </p>
+                        </div>
+                        <div className=" pt-10 w-[43%]">
+                                <img src="/images/p2e.png" />
+                            </div>
+                    </div>
+                    <div className="pt-[10%]">
+                        <div className="text-3xl font-Poppins font-light tracking-wide">
+                            Code
+                        </div>
+                        <div className="font-Roboto pt-5 w-[45%] text-neutral-500">
+                            <p>
+                                I designed in Figma, and coded all with HTML, CSS, JavaScript, and WordPress under the review by an SEO specialist. Implemented 3 post types News, Blog, and Recruit managed in CMS, and Contact form and Job application form that send to email addresses of the company and applicants.
+                            </p>
+                        </div>
+                        <div className="font-Roboto pt-10 w-[45%] text-neutral-500">
+                            <img src="/images/code.png" />
+                        </div>
+                    </div>
+                </div>
+    </div> 
       
   );
   
---
+++ src/pages/Club.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Project2 from '../components/screens/projects/Project2'
+import Header from '../components/header/Header'
+
+const Club = () => (
+	<div className="w-full h-full ">
+		<Header />
+		<Project2 />
+      </div>
+      
+  );
+  
+  export default Club;
