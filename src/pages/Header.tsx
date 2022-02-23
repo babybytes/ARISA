1d1a25216431baa2d54125191814757957c70de2
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-23 12:45:26 +0530

    changes

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -5,9 +5,11 @@ import Link from 'next/link'
 const Header = () => (
 	<div className=" fixed w-full z-10">
 		<div className="flex pt-10 justify-around space-x-[35%] ">
-			<div className="">
-				<img src="/images/logo_arisa.svg" />
-			</div>
+			<Link href="/" >
+				<div className="cursor-pointer">
+					<img src="/images/logo_arisa.svg" />
+				</div>
+			</Link>
 			<Link href="/Ace">
 				<a  className="text-neutral-300 pt-1 text-2xl ">
 					About
