515c1add0c42c51dfc73abda37dfe4811eb160e4
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-25 01:21:04 +0530

    projects responsive

--- src/components/screens/projects/Project4.tsx
+++ src/components/screens/projects/Project4.tsx
@@ -5,11 +5,11 @@ import React from 'react';
 const Project4 = () => (
 	<div className="w-full h-full text-white">
 		<div>
-			<div className="w-full h-full flex  flex-col items-center pt-[7%] space-y-5 ">
-				<div className="text-neutral-400 font-Roboto tracking-[0.2rem] text-sm">
+			<div className="w-full h-full flex  flex-col items-center lg:pt-[7%] pt-[30%] md:pt-[15%] space-y-5 ">
+				<div className="text-neutral-400 font-Roboto tracking-[0.2rem] lg:text-sm text-xs">
 					B2B Streaming Product
 				</div>
-				<div className="text-8xl w-[70%] text-center  italic font-bold font-Poppins">
+				<div className="lg:text-8xl text-4xl lg:w-[60%] w-[95%] text-center  italic font-bold font-Poppins">
 					Designed app, web admin, and marketing website
 				</div>
 				<div className="pt-[3%]   ">
@@ -17,12 +17,12 @@ const Project4 = () => (
 				</div>
 			</div>
 		</div>
-		<div className="flex pt-[15%]  w-full justify-around space-x-[5%] ">
+		<div className="lg:flex  pt-[15%]  w-full lg:justify-center lg:space-x-[35%] space-x-[5%]  ">
 			<div>
-				<div className="text-6xl font-Poppins font-semibold"> 
+				<div className="lg:text-6xl  text-4xl font-Poppins font-semibold md:ml-[5%] ml-4 lg:ml-0"> 
 					Summary
 				</div>
-				<div className="pt-[10%] text-neutral-400">
+				<div className="pt-[10%] text-neutral-400 hidden lg:block">
 					<button className="border flex rounded-full justify-center items-center w-full h-[3.5rem]">
 						<span className="font-Roboto">
 							Visit WebSite
@@ -34,17 +34,17 @@ const Project4 = () => (
 				</div>
 				
 			</div>
-			<div className="w-[32%]  font-Roboto text-neutral-400 leading-loose">
-				<p className="w-[85%]">
+			<div className="lg:w-[32%] md:w-[90%]  font-Roboto text-neutral-400 leading-loose md:pt-5 pt-2">
+				<p className="lg:w-[85%]">
 					TVplum first launched in December 2020 as a B2B streaming service. It provides a web management screen and viewing application. I took over the UI design of CMS that can organize TV programs and videos, designed a customizable application UI from scratch, and marketing website.
 				</p>
-				<p className="w-[85%]">
+				<p className="lg:w-[85%]">
 					In marketing website, user's staying time has increased 8 times by improving with design sprints.
 				</p>
 			</div>  
 		</div>    
-		<div className="flex justify-around w-full pt-[15%]">
-			<div className="flex flex-col w-full ml-[13%]">
+		<div className="lg:flex lg:justify-around lg:items-start w-full pt-[10%]">
+			<div className="lg:flex lg:flex-col ml-4  md:flex md:flex-row md:justify-around lg:w-[50%] lg:ml-[13%] lg:space-y-[10%]">
 				<div>
 					<div className="font-Poppins text-4xl font-semibold tracking-wide">
 						My Role
@@ -76,7 +76,7 @@ const Project4 = () => (
 						</li>
 					</ul>
 				</div>
-				<div className=" pt-[10%] ">
+				<div className="pt-10  md:pt-0 ">
 					<div className="font-Poppins text-4xl font-semibold tracking-wider">
 						Client
 					</div>
@@ -88,18 +88,18 @@ const Project4 = () => (
 				</div>
 			
 			</div>
-			<div className=" w-full  ">
+			<div className="  lg:w-[50%]  pt-5 lg:pt-0 ">
 				<div className="w-[100%] ">
 					<img src="/images/fourth_page.png" />
 				</div>
 				
 			</div>
 		</div>
-            	<div className="ml-[12.7%] font-Poppins text-4xl font-semibold tracking-wider pt-[5%]">
+            	<div className="lg:ml-[12.7%] font-Poppins md:text-4xl font-semibold tracking-wider  lg:pt-[5%]  pt-5 text-2xl ml-[35%]">
                 	Process
             	</div>
-                <div className="flex    pt-[5%]">
-			<div className="ml-[12.7%]">
+                <div className="lg:flex    md:pt-[5%] ml-4 lg:ml-0">
+			<div className="lg:ml-[12.7%] lg:block hidden">
 				<div className="font-Roboto ">
 					Schedule
 				</div>
@@ -110,31 +110,31 @@ const Project4 = () => (
 					Code
 				</div>
 			</div>
-                   	<div className="ml-[20%]">
+                   	<div className="lg:ml-[20%] md:ml-[5%] pt-10 md:pt-0">
 				<div className="text-3xl font-Poppins font-light tracking-wide">
 					Issue
 				</div>
-				<div className="font-Roboto pt-5 w-[42%] text-neutral-500">
+				<div className="font-Roboto lg:pt-5 lg:w-[43%] md:w-[90%] text-neutral-500 pt-2">
 					<p>
 						Regarding the marketing website for B2B streaming service TVplum, we have received feedback such as "Difficult to understand" and "Do not know how to use it". In order to solve this problem, I have gathered members from various departments to run workshops.
 					</p>
 				</div>
-				<div className=" pt-10 w-[43%]">
+				<div className=" md:pt-10 pt-5 lg:w-[43%] w-[90%]">
 					<img src="/images/p4a.png" />
 				</div>
                     	</div>
                 </div>
-                <div className="ml-[36.2%]">
-			<div className="pt-[15%]">
+                <div className="lg:ml-[36.2%] ml-4 md:ml-[7.5%]">
+			<div className="lg:pt-[15%] pt-[10%]">
 				<div className="text-3xl font-Poppins font-light tracking-wide">
 					Strategy
 				</div>
-				<div className="font-Roboto pt-5 w-[42%] text-neutral-500">
+				<div className="font-Roboto lg:pt-5 lg:w-[43%] md:w-[90%] text-neutral-500 pt-2">
 					<p>
 						Identified positive and negative aspects in the internal and external environment by SWOT analysis on Miro. In addition, since clients who have already reached out to us were only several number, we used the same data as clients as personas.
 					</p>
 				</div>
-				<div className=" pt-10 w-[43%]">
+				<div className=" md:pt-10 pt-5 lg:w-[43%] w-[90%]">
 					<img src="/images/p4b.png" />
 				</div>
 			</div>
@@ -142,12 +142,12 @@ const Project4 = () => (
 				<div className="text-3xl font-Poppins font-light tracking-wide">
 					Scope
 				</div>
-				<div className="font-Roboto pt-5 w-[42%] text-neutral-500">
+				<div className="font-Roboto lg:pt-5 lg:w-[43%] md:w-[90%] text-neutral-500 pt-2">
 					<p>
 						Created customer journey of personas that we made in Strategy also based on facts. 
 					</p>
 				</div>
-				<div className=" pt-10 w-[43%]">
+				<div className=" md:pt-10 pt-5 lg:w-[43%] w-[90%]">
 					<img src="/images/p4c.png" />
 				</div>
 			</div>
@@ -155,12 +155,12 @@ const Project4 = () => (
 				<div className="text-3xl font-Poppins font-light tracking-wide">
 					Structure
 				</div>
-				<div className="font-Roboto pt-5 w-[42%] text-neutral-500">
+				<div className="font-Roboto lg:pt-5 lg:w-[43%] md:w-[90%] text-neutral-500 pt-2">
 					<p>
 						Identified the possible elements, selected necessaries, and then classified them.
 					</p>
 				</div>
-				<div className=" pt-10 w-[43%]">
+				<div className=" md:pt-10 pt-5 lg:w-[43%] w-[90%]">
 					<img src="/images/p4d.png" />
 				</div>
 			</div>
@@ -168,12 +168,12 @@ const Project4 = () => (
 				<div className="text-3xl font-Poppins font-light tracking-wide">
 					Skeleton
 				</div>
-				<div className="font-Roboto pt-5 w-[42%] text-neutral-500">
+				<div className="font-Roboto lg:pt-5 lg:w-[43%] md:w-[90%] text-neutral-500 pt-2">
 					<p>
 						Based on the information architecture, we put detailed contents and user's emotions. And then I created a wireframe on no code tool STUDIO, received feedback from actual clients and people who did not know our service, and repeated modifications.
 					</p>
 				</div>
-				<div className=" pt-10 w-[43%]">
+				<div className=" md:pt-10 pt-5 lg:w-[43%] w-[90%]">
 					<img src="/images/p4e.png" />
 				</div>
 			</div>
@@ -181,12 +181,12 @@ const Project4 = () => (
 				<div className="text-3xl font-Poppins font-light tracking-wide">
 					Surface
 				</div>
-				<div className="font-Roboto pt-5 w-[42%] text-neutral-500">
+				<div className="font-Roboto lg:pt-5 lg:w-[43%] md:w-[90%] text-neutral-500 pt-2">
 					<p>
 						To give an easy-to-understand impression to the personas, I finished it with a generous spacing and a slightly rounded layout.
 					</p>
 				</div>
-				<div className=" pt-10 w-[43%]">
+				<div className=" md:pt-10 pt-5 lg:w-[43%] w-[90%]">
 					<img src="/images/p4f.png" />
 				</div>
 			</div>
@@ -194,24 +194,24 @@ const Project4 = () => (
 				<div className="text-3xl font-Poppins font-light tracking-wide">
 					Result
 				</div>
-				<div className="font-Roboto pt-5 w-[42%] text-neutral-500">
+				<div className="font-Roboto lg:pt-5 lg:w-[43%] md:w-[90%] text-neutral-500 pt-2">
 					<p>
 						When the product was first launched, we were targeting clients in the TV industry, so mentioned the technical functions. In reality, however, clients who have problems with existing streaming platforms or entrepreneurs who considers new business with video were interested in our product. By emphasizing the strength "customizability" which was found in these needs, user's staying time has increased 8 times.
 					</p>
 				</div>
 				<div >
-					<div className=" pt-10 w-[43%]">
+					<div className=" md:pt-10 pt-5 lg:w-[43%] w-[90%]">
 						<img src="/images/p4g.png" />
 					</div>
-					<div className="font-Roboto pt-5  text-neutral-500 ml-[20%] ">
+					<div className="font-Roboto pt-5  text-neutral-500 lg:ml-[20%] ml-[40%] md:ml-[40%] ">
 						Before
 					</div>
                         	</div>
                         	<div>
-					<div className=" pt-10 w-[43%]">
+					<div className="md:pt-10 pt-5 lg:w-[43%] w-[90%] ">
 						<img src="/images/p4h.png" />
 					</div>
-					<div className="font-Roboto pt-5  text-neutral-500 ml-[20%]">
+					<div className="font-Roboto pt-5  text-neutral-500 lg:ml-[21%] ml-[40%] md:ml-[40%]">
 						After
 					</div>
                         	</div>
