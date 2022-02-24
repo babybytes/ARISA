ebc4c4b2518946def4eaf9f7f59d3395dbec2b9d
shelabh <68127067+shelabh@users.noreply.github.com>
2022-02-24 19:15:50 +0530

    Response

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 
 const Footer = () => (
-	<div className="w-full h-full pt-[20%]">
-		<div className="w-full "> 
-			<div className="">
+	<div className="w-full h-full ">
+		<div className="w-full h-full pt-[20%] md:pt-[10%]"> 
+			<div className="lg:ml-[20%] lg:w-[59%]">
 				<img src="/images/divider.png" />
 			</div>
 			<div className="flex justify-around text-white h-[5rem] items-center">
 				<div className=" font-Poppins ">
 					arisa120597@gmail.com
 				</div>
-				<div className="flex w-[25%]">
+				<div className="flex w-[25%] md:w-[15%] lg:w-[5%]">
 					<div className="w-full">
 						<img src="/images/icon_linkedin.png" />
 					</div>
